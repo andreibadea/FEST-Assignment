f653dfa6f9e91b2ab6678359bc9b8341876c9057
andreibadea <57230506+andreibadea@users.noreply.github.com>
2020-11-14 20:05:00 +0100

    Mostly done, added option to login and signup, also with google, facebook and twitter, but the last 2 dont work 100%. NEED TO FIX!

--- Login Page/js/app.js
+++ Login Page/js/app.js
@@ -1,3 +1,125 @@
-$(document).ready(function() {});
-
+// $(document).ready(function() {});
 AOS.init();
+
+
+googleSignIn=()=>{
+    var provider = new firebase.auth.GoogleAuthProvider();
+    firebase.auth().signInWithPopup(provider).then(function(result) {
+
+
+        // If it works redirect to the next page!!
+
+
+
+      }).catch(function(error) {
+        // Handle Errors here.
+
+
+        var errorCode = error.code;
+        var errorMessage = error.message;
+        console.log(errorCode);
+        console.log(errorMessage);
+      });
+}
+
+
+
+twitterSignIn=()=>{
+    var provider = new firebase.auth.TwitterAuthProvider();
+    firebase.auth().signInWithPopup(provider).then(function(result) {
+
+
+        // If it works redirect to the next page!!
+
+
+
+      }).catch(function(error) {
+        // Handle Errors here.
+
+
+        var errorCode = error.code;
+        var errorMessage = error.message;
+        console.log(errorCode);
+        console.log(errorMessage);
+      });
+}
+
+
+facebookSignIn=()=>{
+    var provider = new firebase.auth.FacebookAuthProvider();
+    firebase.auth().signInWithPopup(provider).then(function(result) {
+
+
+        // If it works redirect to the next page!!
+
+
+
+      }).catch(function(error) {
+        // Handle Errors here.
+
+
+        var errorCode = error.code;
+        var errorMessage = error.message;
+        console.log(errorCode);
+        console.log(errorMessage);
+      });
+}
+
+
+signUp=()=>{
+
+    const email = document.getElementById('exampleInputEmail1').value;
+    const password = document.getElementById('exampleInputPassword1').value;
+
+    if(email == "" || password == "") {
+        document.getElementById('alert').style.display = "initial";
+    } else {
+        document.getElementById('alert').style.display = "none";
+        firebase.auth().createUserWithEmailAndPassword(email, password).catch(function(error) {
+            
+            document.getElementById('alert').style.display = "initial";
+
+            var errorCode = error.code;
+            var errorMessage = error.message;
+
+            console.log(errorCode);
+            console.log(errorMessage);
+            });
+    }
+    
+
+    
+}
+
+signIn=()=>{
+    const email = document.getElementById('exampleInputEmail1').value;
+    const password = document.getElementById('exampleInputPassword1').value;
+
+    if(email == "" || password == "") {
+        document.getElementById('alert').style.display = "initial";
+    } else {
+        document.getElementById('alert').style.display = "none";
+
+        firebase.auth().signInWithEmailAndPassword(email, password).catch(function(error) {
+            
+            document.getElementById('alert').style.display = "initial";
+
+            var errorCode = error.code;
+            var errorMessage = error.message;
+
+            console.log(errorCode);
+            console.log(errorMessage);
+          });
+    }
+
+    
+}
+
+
+signOut=()=>{
+    firebase.auth().signOut().then(function() {
+        // Sign-out successful.
+      }).catch(function(error) {
+        // An error happened.
+      });
+}
